3da1b4c088daed9ba5dc4d10131cb18251844781
Jonathan Forson <jonathanforsondanso@gmail.com>
2024-04-13 14:39:35 +0000

    Initialise script for navbar

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,6 @@
+// Initialising script for nav bar
+document.addEventListener('DOMContentLoaded', function() {
+   let sidenav = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sidenav);
+  });
+
